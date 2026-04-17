b51d8ff99426702bc548cd0aec86b851864d2d5a
CosmicElvin <addmc94@gmail.com>
2026-04-17 12:44:27 -0500

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,14 +1,22 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged, User } from 'firebase/auth';
 import { getFirestore, doc, setDoc, getDoc, collection, query, onSnapshot, orderBy, addDoc, updateDoc, deleteDoc, serverTimestamp, Timestamp } from 'firebase/firestore';
-import firebaseConfig from '../../firebase-applet-config.json';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCZlEn4_7uwPqnwr53H8J8hHIAyw7hxjxY",
+  authDomain: "draftslide.firebaseapp.com",
+  projectId: "draftslide",
+  storageBucket: "draftslide.firebasestorage.app",
+  messagingSenderId: "802991323978",
+  appId: "1:802991323978:web:006468d96210d8be3fdfb3",
+  measurementId: "G-C2HRXEW5L6"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
+export const db = getFirestore(app);
 export const googleProvider = new GoogleAuthProvider();
-
 // Auth Helpers
 export const signInWithGoogle = async () => {
   try {
